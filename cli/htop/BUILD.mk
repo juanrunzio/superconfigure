f814755bd36e1b4dd8a138717559cd26d1c24270
juanchitho <jbauti9@gmail.com>
2024-06-11 10:34:33 -0300

    fix in the BUILD.mk

--- cli/htop/BUILD.mk
+++ cli/htop/BUILD.mk
@@ -1,12 +1,12 @@
-HTOP_SRC := https://github.com/htop-dev/htop/archive/refs/tags/3.3.0.tar.gz
+HTOP_SRC := https://github.com/htop-dev/htop/archive/refs/tags/33300.tar.gz
 HTOP_DEPS := lib/ncurses
 
 $(eval $(call DOWNLOAD_SOURCE,cli/htop,$(HTOP_SRC)))
 $(eval $(call SPECIFY_DEPS,cli/htop,$(HTOP_DEPS)))
 
 o/cli/htop/setup: o/cli/htop/patched
-    cd $(BASELOC)/o/cli/htop/htop* && ./autogen.sh
-    touch $@
+	@cd $(BASELOC)/o/cli/htop/htop* && ./autogen.sh
+	@touch $@
 
 o/cli/htop/configured.x86_64: o/cli/htop/setup
 o/cli/htop/configured.aarch64: o/cli/htop/setup
