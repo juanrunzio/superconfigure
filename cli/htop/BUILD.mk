95b135455473ad21267d026208046e183aa5a0d4
juanchitho <jbauti9@gmail.com>
2024-06-27 18:06:56 -0300

    testing with a patch

--- cli/htop/BUILD.mk
+++ cli/htop/BUILD.mk
@@ -3,16 +3,16 @@ HTOP_DEPS := lib/ncurses
 $(eval $(call DOWNLOAD_SOURCE,cli/htop,$(HTOP_SRC)))
 $(eval $(call SPECIFY_DEPS,cli/htop,$(HTOP_DEPS)))
 o/cli/htop/source:
-    mkdir -p $(BASELOC)/o/cli/htop
-    wget -O $(BASELOC)/o/cli/htop/source.tar.gz $(HTOP_SRC)
-    tar -xzf $(BASELOC)/o/cli/htop/source.tar.gz -C $(BASELOC)/o/cli/htop
-    touch $@
+	mkdir -p $(BASELOC)/o/cli/htop
+	wget -O $(BASELOC)/o/cli/htop/source.tar.gz $(HTOP_SRC)
+	tar -xzf $(BASELOC)/o/cli/htop/source.tar.gz -C $(BASELOC)/o/cli/htop
+	touch $@
 o/cli/htop/patched: o/cli/htop/source
-    cd $(BASELOC)/o/cli/htop/htop* && patch -p1 < $(BASELOC)/patches/fix-scheduling.patch
-    touch $@
+	cd $(BASELOC)/o/cli/htop/htop* && patch -p1 < $(BASELOC)/patches/fix-scheduling.patch
+	touch $@
 o/cli/htop/setup: o/cli/htop/patched
-    cd $(BASELOC)/o/cli/htop/htop* && ./autogen.sh
-    touch $@
+	cd $(BASELOC)/o/cli/htop/htop* && ./autogen.sh
+	touch $@
 o/cli/htop/configured.x86_64: o/cli/htop/setup
 o/cli/htop/configured.aarch64: o/cli/htop/setup
 o/cli/htop/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/cli/htop/config-wrapper
