e939e31343adae88bf0d2247860b9150e8a7d87c
juanchitho <jbauti9@gmail.com>
2024-06-27 17:24:51 -0300

    testing with a patch

--- cli/htop/BUILD.mk
+++ cli/htop/BUILD.mk
@@ -1,16 +1,17 @@
+
 HTOP_SRC := https://github.com/htop-dev/htop/archive/refs/tags/3.3.0.tar.gz
 HTOP_DEPS := lib/ncurses
 
 $(eval $(call DOWNLOAD_SOURCE,cli/htop,$(HTOP_SRC)))
 $(eval $(call SPECIFY_DEPS,cli/htop,$(HTOP_DEPS)))
 
-o/cli/htop/patched: o/cli/htop/patched
-	cd $(BASELOC)/o/cli/htop/htop* && patch -p1 < $(BASELOC)/patches/fix-scheduling.patch
-	touch $@
+o/cli/htop/patched: o/cli/htop/source
+    cd $(BASELOC)/o/cli/htop/htop* && patch -p1 < $(BASELOC)/patches/fix-scheduling.patch
+    touch $@
 
 o/cli/htop/setup: o/cli/htop/patched
-	cd $(BASELOC)/o/cli/htop/htop* && ./autogen.sh
-	touch $@
+    cd $(BASELOC)/o/cli/htop/htop* && ./autogen.sh
+    touch $@
 
 o/cli/htop/configured.x86_64: o/cli/htop/setup
 o/cli/htop/configured.aarch64: o/cli/htop/setup
