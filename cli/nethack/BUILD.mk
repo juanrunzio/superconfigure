8396b1d10f53391b2d1f8c81e6d028dbe198f844
juanrunzio <jbauti@gmail.com>
2024-10-23 19:13:58 -0300

    testing a different signature

--- cli/nethack/BUILD.mk
+++ cli/nethack/BUILD.mk
@@ -2,7 +2,7 @@
 NETHACK_SRC := https://www.nethack.org/download/3.6.7/nethack-367-src.tgz
 
 $(eval $(call DOWNLOAD_SOURCE,cli/nethack,$(NETHACK_SRC)))
-$(eval $(call SPECIFY_DEPS,cli/nethack,lib/ncurses,compress/gzip))
+$(eval $(call SPECIFY_DEPS,cli/nethack,lib/ncurses compress/gzip))
 
 o/cli/nethack/configured.x86_64:\
 	CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
