aa59c8859b1ae916b1676db208871cf154b677eb
rladaga <93140602+rladaga@users.noreply.github.com>
2024-11-04 14:49:11 -0300

    rewrite for another test

--- cli/nethack/BUILD.mk
+++ cli/nethack/BUILD.mk
@@ -1,15 +1,12 @@
 
 NETHACK_SRC := https://github.com/NetHack/NetHack/archive/refs/tags/NetHack-3.6.7_Released.tar.gz
+NETHACK_DEPS := lib/ncurses
 
 $(eval $(call DOWNLOAD_SOURCE,cli/nethack,$(NETHACK_SRC)))
-$(eval $(call SPECIFY_DEPS,cli/nethack,lib/ncurses))
+$(eval $(call SPECIFY_DEPS,cli/nethack,$(NETHACK_DEPS)))
 
-o/cli/nethack/configured.x86_64:\
-	CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
-
-o/cli/nethack/configured.aarch64:\
-	CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
+o/cli/nethack/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
+o/cli/nethack/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
 
 o/cli/nethack/built.fat: FATTEN_COMMAND = $(BASELOC)/config/apelink_folder.sh
-o/cli/nethack/built.fat: \
-	BINS = nethack usr/share/terminfo usr/share/nethack
+o/cli/nethack/built.fat: BINS = nethack usr/share/terminfo
