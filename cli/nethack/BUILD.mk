38610e2b728ed6ca2d99952e26d69cda6b364b14
juanrunzio <jbauti@gmail.com>
2024-12-14 16:44:25 -0300

    testing

--- cli/nethack/BUILD.mk
+++ cli/nethack/BUILD.mk
@@ -6,15 +6,15 @@ $(eval $(call DOWNLOAD_SOURCE,cli/nethack,$(NETHACK_SRC)))
 $(eval $(call SPECIFY_DEPS,cli/nethack,$(NETHACK_DEPS)))
 
 o/cli/nethack/configured.x86_64:
-	mkdir -p $(BASELOC)/zip/cfg
-	$(CONFIG_COMMAND)
+	mkdir -p $(BASELOC)/cli/nethack/zip/cfg
+	$(BASELOC)/cli/nethack/config-wrapper
 
 o/cli/nethack/configured.aarch64:
-	mkdir -p $(BASELOC)/zip/cfg
-	$(CONFIG_COMMAND)
+	mkdir -p $(BASELOC)/cli/nethack/zip/cfg
+	$(BASELOC)/cli/nethack/config-wrapper	
 
-o/cli/nethack/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
-o/cli/nethack/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
+#o/cli/nethack/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
+#o/cli/nethack/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
 
 o/cli/nethack/built.fat: FATTEN_COMMAND = $(BASELOC)/config/apelink_folder.sh
 o/cli/nethack/built.fat: BINS = nethack usr/share/terminfo
