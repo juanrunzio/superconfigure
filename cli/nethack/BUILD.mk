52d99a4344d67b8a5ea49049ea3f40a49eacfea9
juanrunzio <jbauti@gmail.com>
2024-12-14 17:09:46 -0300

    testing2

--- cli/nethack/BUILD.mk
+++ cli/nethack/BUILD.mk
@@ -5,16 +5,13 @@ NETHACK_DEPS := lib/ncurses
 $(eval $(call DOWNLOAD_SOURCE,cli/nethack,$(NETHACK_SRC)))
 $(eval $(call SPECIFY_DEPS,cli/nethack,$(NETHACK_DEPS)))
 
-o/cli/nethack/configured.x86_64:
+create_dir:
 	mkdir -p $(BASELOC)/cli/nethack/zip/cfg
-	$(BASELOC)/cli/nethack/config-wrapper
 
-o/cli/nethack/configured.aarch64:
-	mkdir -p $(BASELOC)/cli/nethack/zip/cfg
-	$(BASELOC)/cli/nethack/config-wrapper	
-
-#o/cli/nethack/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
-#o/cli/nethack/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
+o/cli/nethack/configured.x86_64: create_dir
+	CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
+o/cli/nethack/configured.aarch64: create_dir
+	CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
 
 o/cli/nethack/built.fat: FATTEN_COMMAND = $(BASELOC)/config/apelink_folder.sh
 o/cli/nethack/built.fat: BINS = nethack usr/share/terminfo
