02b6c60f488b6baabff43c609a4ed1a9b0f8e1fa
juanrunzio <jbauti@gmail.com>
2024-12-14 18:42:51 -0300

    fix folder

--- cli/nethack/BUILD.mk
+++ cli/nethack/BUILD.mk
@@ -5,13 +5,8 @@ NETHACK_DEPS := lib/ncurses
 $(eval $(call DOWNLOAD_SOURCE,cli/nethack,$(NETHACK_SRC)))
 $(eval $(call SPECIFY_DEPS,cli/nethack,$(NETHACK_DEPS)))
 
-create_dir:	
-	mkdir -p $(BASELOC)/cli/nethack/zip/cfg
-
-o/cli/nethack/configured.x86_64: create_dir
-	$(BASELOC)/cli/nethack/config-wrapper
-o/cli/nethack/configured.aarch64: create_dir
-	$(BASELOC)/cli/nethack/config-wrapper
+o/cli/nethack/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
+o/cli/nethack/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/cli/nethack/config-wrapper
 
 o/cli/nethack/built.fat: FATTEN_COMMAND = $(BASELOC)/config/apelink_folder.sh
 o/cli/nethack/built.fat: BINS = nethack usr/share/terminfo
