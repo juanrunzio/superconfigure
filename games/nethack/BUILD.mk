8684856d3c6fa14a055167329ff2a31930ba41d5
rladaga <93140602+rladaga@users.noreply.github.com>
2025-01-31 13:50:42 -0300

    test

--- games/nethack/BUILD.mk
+++ games/nethack/BUILD.mk
@@ -5,8 +5,8 @@ NETHACK_DEPS := lib/ncurses
 $(eval $(call DOWNLOAD_SOURCE,games/nethack,$(NETHACK_SRC)))
 $(eval $(call SPECIFY_DEPS,games/nethack,$(NETHACK_DEPS)))
 
-o/games/nethack/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/games/nethack/config-wrapper
-o/games/nethack/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/games/nethack/config-wrapper
+o/games/nethack/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/games/nethack/config-wrapper x86_64
+o/games/nethack/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/games/nethack/config-wrapper aarch64
 
-o/games/nethack/built.fat: FATTEN_COMMAND = $(BASELOC)/games/nethack/fatten
-#o/games/nethack/built.fat: BINS = nethack share/terminfo sysconf nhdat
+o/games/nethack/built.fat: FATTEN_COMMAND = $(BASELOC)/config/apelink_folder.sh
+o/games/nethack/built.fat: BINS = nethack share/terminfo sysconf nhdat
